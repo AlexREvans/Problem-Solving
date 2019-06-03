bc39a718ec687984d82d6c86621e855724744188
AlexREvans <AlexREvans@users.noreply.github.com>
2019-06-03 10:49:58 +0100

    Add test that checks a variety of inputs

---
+++ src/test/safe.solveany.ts
@@ -0,0 +1,40 @@
+// Reference mocha-typescript's global definitions:
+/// <reference path="../node_modules/mocha-typescript/globals.d.ts" />
+
+import { expect } from "chai";
+import { CodeGenerator, CodeFeedback } from "../main/safe/safe";
+import { solve, FeedbackSupplier, Threshold } from "../main/solver/solver";
+
+
+const usefulFeedback = function (theCode: string): FeedbackSupplier<string, CodeFeedback> {
+    const chars = theCode.split('');
+    return candidate => ({
+        indexMatches:
+            chars.map((digit, index) => candidate[index] === digit)
+    });
+};
+
+const matchingCode = function (theCode: string): Threshold<string, any> {
+    return candidate => candidate === theCode;
+};
+
+describe("Solving for different inputs", () => {
+    [
+        '5352',
+        '432',
+        '43',
+        '41',
+        '115',
+        '46242',
+        '110032'
+    ].forEach(theCode => {
+        it("can solve for " + theCode, () => {
+            const result = solve(
+                new CodeGenerator(theCode.length),
+                usefulFeedback(theCode),
+                matchingCode(theCode));
+
+            expect(result.solution).to.eq(theCode);
+        })
+    })
+});
