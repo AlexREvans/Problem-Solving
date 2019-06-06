8c8844c4dcd174423c2d364ba1c4489acfd24ffd
AlexREvans <AlexREvans@users.noreply.github.com>
2019-06-06 22:00:26 +0100

    Reformat solver

--- src/main/solver.ts
+++ src/main/solver.ts
@@ -3,9 +3,18 @@ export interface Generator<ANSWER, FEEDBACK> {
     hasNextCandidate(): boolean;
     receiveFeedback(candidate: ANSWER, feedback: FEEDBACK): void;
 }
-export type FeedbackSupplier<ANSWER, FEEDBACK> = (candidate: ANSWER) => FEEDBACK
-export type Threshold<ANSWER, FEEDBACK> = (answer: ANSWER, feedback: FEEDBACK) => boolean;
-export type AnswerWithStats<ANSWER> = { answer: ANSWER, answersConsidered: number }
+
+export type FeedbackSupplier<ANSWER, FEEDBACK> =
+    (candidate: ANSWER) => FEEDBACK
+
+export type Threshold<ANSWER, FEEDBACK> =
+    (answer: ANSWER, feedback: FEEDBACK) => boolean;
+
+export type AnswerWithStats<ANSWER> =
+    { answer: ANSWER, answersConsidered: number }
+
+
+
 
 
 export function findAnswer<ANSWER, FEEDBACK>(
@@ -16,13 +25,20 @@ export function findAnswer<ANSWER, FEEDBACK>(
     var answersConsidered = 0;
 
     while (gen.hasNextCandidate()) {
-        const answer: ANSWER = gen.nextCandidate();                                 /* STEP 1 - ABILITY TO GENERATE ANSWERS */
+
+        /* STEP 1 - ABILITY TO GENERATE ANSWERS */
+        const answer: ANSWER = gen.nextCandidate();
         answersConsidered += 1;
-        const feedback: FEEDBACK = feedbackSupplier(answer);                        /* STEP 2 - ABILITY TO ASSESS ANSWERS */
+
+        /* STEP 2 - ABILITY TO ASSESS ANSWERS */
+        const feedback: FEEDBACK = feedbackSupplier(answer);
         if (threshold(answer, feedback)) {
             return { answer: answer, answersConsidered: answersConsidered }
         }
-        gen.receiveFeedback(answer, feedback);                                      /* STEP 3 - ABILITY TO PROCESS FEEDBACK */
+
+        /* STEP 3 - ABILITY TO PROCESS FEEDBACK */
+        gen.receiveFeedback(answer, feedback);
     }
+
     return { answer: null, answersConsidered: answersConsidered };
 }
